9aa88a88d5815429a628384f779480b995858a4f
Farkass47 <sandor.farkas@t-online.hu>
2019-07-04 21:44:50 +0200

    Updates

---
+++ precache-manifest.25a36346990d27730acdcbb172cb70e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5349140f68a0258dea8aab5690b47603",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d1263d68f6a17ca73d7e",
+    "url": "/monsters-rolodex/static/css/main.db49be09.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/monsters-rolodex/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "d1263d68f6a17ca73d7e",
+    "url": "/monsters-rolodex/static/js/main.dcda662f.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
